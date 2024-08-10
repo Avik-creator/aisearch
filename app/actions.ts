786ca1b058fc4e1bb91fc8c7a08a01b173c77365
Avik-creator <avikm744@gmail.com>
2024-08-10 23:10:07 +0530

    Feat: Added API Route.

---
+++ app/actions.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { generateObject } from "ai";
+import { google } from "@ai-sdk/google";
+import { z } from "zod";
+
+export interface Message {
+  role: "user" | "assistant";
+  content: string;
+}
+
+export async function suggestQuestions(history: Message[]) {
+  const { object } = await generateObject({
+    model: google("models/gemini-1.5-flash-latest"),
+    temperature: 0.5,
+    system: `You are a search engine query generator. You 'have' to create 3 questions for the search engine based on the message history which has been provided to you.
+The questions should be open-ended and should encourage further discussion while maintaining the whole context. Limit it to 5-10 words per question. 
+Always put the user input's context is some way so that the next search knows what to search for exactly.
+Never use pronouns in the questions as they blur the context.`,
+    messages: history,
+    schema: z.object({
+      questions: z
+        .array(z.string())
+        .describe("The Generated Questions based on the Message History."),
+    }),
+  });
+
+  return {
+    questions: object.questions,
+  };
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,116 @@
-import Image from "next/image";
+import { openai, createOpenAI } from "@ai-sdk/openai";
+import { mistral } from "@ai-sdk/mistral";
+import { z } from "zod";
+import { anthropic } from "@ai-sdk/anthropic";
+import { convertToCoreMessages, streamText, tool } from "ai";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+// Allowing API calls to extend beyond normal limit.
+export const maxDuration = 60;
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const groq = createOpenAI({
+  baseURL: "https://api.groq.com/openai/v1",
+  apiKey: process.env.GROQ_API_KEY,
+});
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export async function POST(req: Request) {
+  const { messages, model } = await req.json();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  let ansModel;
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  if (model == "llama-3.1-405b-reasoning") {
+    ansModel = groq("llama-3.1-405b-reasoning");
+  } else if (model == "mistral-large-latest") {
+    ansModel = mistral("mistral-large-latest");
+  } else if (model == "claude-3-5-sonnet-20240620") {
+    ansModel = anthropic("claude-3-5-sonnet-20240620");
+  } else {
+    ansModel = model;
+  }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  const result = await streamText({
+    model: ansModel,
+    temperature: 0.5,
+    messages: convertToCoreMessages(messages),
+    system:
+      "You are an AI web search engine that helps users find information on the internet." +
+      "You use the 'web_search' tool to search for information on the internet." +
+      "Always call the 'web_search' tool to get the information, no need to do a chain of thought or say anything else, go straight to the point." +
+      "Once you have found the information, you provide the user with the information you found in brief like a news paper detail." +
+      "The detail should be 3-5 paragraphs in 10-12 sentences, some time pointers, each with citations in the [Text](link) format always!" +
+      "Citations can be inline of the text like this: Hey there! [Google](https://google.com) is a search engine." +
+      "Do not start the responses with newline characters, always start with the first sentence." +
+      "The current date is: " +
+      new Date()
+        .toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "2-digit",
+          weekday: "short",
+        })
+        .replace(/(\w+), (\w+) (\d+), (\d+)/, "$4-$2-$3 ($1)") +
+      +"Never use the heading format in your response!." +
+      "Refrain from saying things like 'Certainly! I'll search for information about OpenAI GEMINI Flash using the web search tool.'",
+    tools: {
+      web_search: tool({
+        description:
+          "Search the web for information with the given query, max results and search depth.",
+        parameters: z.object({
+          query: z.string().describe("The Search Query to look for in the web"),
+          maxResults: z
+            .number()
+            .describe("The maximum number of results to return"),
+          searchDepth: z
+            .enum(["basic", "advanced"])
+            .describe("The search depth to use for the search"),
+        }),
+        execute: async ({
+          query,
+          maxResults,
+          searchDepth,
+        }: {
+          query: string;
+          maxResults: number;
+          searchDepth: "basic" | "advanced";
+        }) => {
+          const apiKey = process.env.TAVILY_API_KEY;
+          const response = await fetch("https://api.tavily.com/search", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              api_key: apiKey,
+              query,
+              max_results: maxResults < 5 ? 5 : maxResults,
+              search_depth: searchDepth,
+              include_images: true,
+              include_answers: true,
+            }),
+          });
+
+          const data = await response.json();
+
+          let context = data.results.map(
+            (obj: { url: any; content: any; title: any; raw_content: any }) => {
+              return {
+                url: obj.url,
+                title: obj.title,
+                content: obj.content,
+                raw_content: obj.raw_content,
+              };
+            }
+          );
+
+          return {
+            results: context,
+          };
+        },
+      }),
+    },
+    onFinish: async (messages) => {
+      console.log("Hello");
+    },
+  });
+
+  return result.toAIStreamResponse();
 }
